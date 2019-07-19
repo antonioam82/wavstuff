6f2025e47c5d682de2f62b74be27b8e38d7686f2
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-07-19 19:36:19 +0200

    Update wav_info.py

--- wav_info.py
+++ wav_info.py
@@ -1,20 +1,40 @@
 import wave
 from VALID import ny
 import subprocess
+import os
+
+def direct():
+    while True:
+        dire = input("Enter directory: ")
+        if os.path.isdir(dire):
+            os.chdir(dire)
+            break
+        print("DIRECTORY NOT FOUNDED")
+
+def filefind():
+    while True:
+        filee=(input("Enter wav file: "))
+        if filee in os.listdir():
+            return filee
+            break
+        print("FILE NOT FOUNDED")
 
 while True:
-    print("*********WAV INFO*********\n")
-    file=input("Enter wav file: ")
+    print("**************WAV INFO**************\n")
+    direct()
+    file=filefind()
     with wave.open(file) as w:
         framerate = w.getframerate()
         frames = w.getnframes()
         channels = w.getnchannels()
         width = w.getsampwidth()
+        data = len(w.readframes(frames))
         
         print('\nSampling Rate: ',framerate, 'Hz')
         print('Length: ',frames, 'samples')
         print('Channels: ',channels)
-        print('Sample Width: ',width, 'bytes\n')
+        print('Sample Width: ',width, 'bytes')
+        print('Buffer Size: ',data, 'bytes\n')
 
         conti=ny(input('Continue?: '))
         if conti=="n":
